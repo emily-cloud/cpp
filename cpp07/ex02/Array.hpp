3785a435fd35318c52cd4e064f9dafefe7897a6d
huahua <emilyaiai86@gmail.com>
2025-03-08 12:56:22 +0100

    cpp07 is fininehed

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -1,10 +1,13 @@
 #ifndef ARRAY_HPP
 # define ARRAY_HPP
 
+#include <iostream>
+#include <exception>
+
 template <typename T> class Array
 {
     private:
-        T *ptr;
+        T *array;
         int size;
 
     public:
@@ -12,9 +15,106 @@ template <typename T> class Array
         Array(int n);
         Array( Array const &src);
         ~Array();
-        Array & operator=(Array const &src)
+        Array & operator=(Array const &src);
 
-        exception::throw()
+        T &operator[](int index);
+        const T &operator[](int index) const;
+        int getSize() const;
+        class OutOfBound: public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
 };
+
+
+template <typename T>
+ Array<T>::Array()
+{
+    array = NULL;
+    size = 0;
+}
+
+template <typename T> 
+Array<T>::Array(int n)
+{
+    size = n;
+    if (n <= 0)
+    {
+        array = NULL;
+        size = 0;
+    }
+    else
+    {
+         array = new T[n]();
+         size = n;
+    }      
+}
+
+
+template <typename T> 
+Array<T>::Array(Array<T> const &src)
+{
+    size = src.size;
+    array = new T[size];
+
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = src.array[i];
+    }
+}
+
+template <typename T>
+ Array<T>::~Array()
+{
+    delete[] array;
+}
+
+
+template <typename T> 
+Array<T> &Array<T>::operator=(Array<T> const &src)
+{
+    if(this != src)
+    {
+        delete[] array;
+        size = src.size;
+    
+        array = new T[size];
+        for(int i = 0; i < size; i++)
+        {
+            array[i] = src.array[i];
+        }
+    }
+    return (*this);
+}
+
+template <typename T>
+int Array<T>::getSize() const
+{
+    return size;
+}
+
+template <typename T>
+T &Array<T>::operator[](int index)
+{
+    if (index < 0 || index >= size)
+        throw OutOfBound();
+    return array[index];
+}
+
+// Operator [] Overload (Const)
+template <typename T>
+const T &Array<T>::operator[](int index) const
+{
+    if (index < 0 || index >= size)
+        throw OutOfBound();
+    return array[index];
+}
+
+template <typename T> 
+const char* Array<T>::OutOfBound::what() const throw()
+{
+    return ("Error: Out of bound");
+}
 
 #endif
---
+++ cpp07/ex02/main.cpp
@@ -0,0 +1,36 @@
+
+#include "Array.hpp"
+
+int main()
+{
+    Array<int> intArray(5);
+    Array<float> floatArray(5);
+    Array<double> doubleArray(5);
+
+    for (int i = 0; i < 5; i++)
+    {
+        intArray[i] = i;
+        floatArray[i] = i + 0.5;
+        doubleArray[i] = i + 0.5;
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        std::cout << "intArray[" << i << "] = " << intArray[i] << std::endl;
+        std::cout << "floatArray[" << i << "] = " << floatArray[i] << std::endl;
+        std::cout << "doubleArray[" << i << "] = " << doubleArray[i] << std::endl;
+        std::cout << std::endl;
+    }
+
+    try
+    {
+        intArray[6] = 42;
+    }
+    catch (std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+
+    return 0;
+
+}
