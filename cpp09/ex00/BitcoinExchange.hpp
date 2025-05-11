cc36ed93fb28f422b3adce42c89dde3d621b5017
emily-cloud <emilyaiai86@gmail.com>
2025-05-11 17:24:26 +0200

    ex00 is finished

--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -1,18 +1,35 @@
-// ex00 use array ex01 list, ex02 vector qeueu stack
 
+// ex00 use map ex01 list, ex02 vector qeueu stack
 #ifndef BITCOINEXCHANGE_HPP
 # define BITCOINEXCHANGE_HPP
 
 #include <iostream>
-#include <array>
 #include <algorithm>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <map>
+#include <cstdlib>
+#include <iomanip>
+#include <cctype>
 
-template <typename T> class BitcoinExchange
+
+
+class BitcoinExchange
 {
+private:
+    std::map<std::string, double> data;
 
-
+public:
+    BitcoinExchange();
+    BitcoinExchange(const BitcoinExchange &other);
+    BitcoinExchange &operator=(const BitcoinExchange &other);
+    ~BitcoinExchange();
+    void readData();
+    void debugPrintData() const;
+    void calculatePrintExchange(const std::string &filename);
+    bool validateInput(std::string date, double value);
 };
-
 
 
 #endif
